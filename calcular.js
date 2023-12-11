93725b34c1e30f801493cab0bf2518bfb73cf669
FabianCiceia <83356320+FabianCiceia@users.noreply.github.com>
2023-12-10 21:09:49 -0300

    Add files via upload

--- calcular.js
+++ calcular.js
@@ -1,36 +1,46 @@
-const BOTON = document.getElementById('sumar_boton');
+const BOTON = document.getElementById('sumarBoton');
 const FLU = document.getElementById('flu');
 const MAN = document.getElementById('man');
-const ENTRADA = document.getElementById('peso');
-const ERROR = document.getElementById('mensaje_error')
-BOTON.addEventListener('click',calcular_peso);
+const ENTRADA = document.getElementById('pesoInput');
+const ERROR = document.getElementById('mensajeError')
 
-function calcular_peso(){
-    const peso_obtenido = parseInt(ENTRADA.value);
+BOTON.addEventListener('click',calcularPeso);
+
+ENTRADA.addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        calcularPeso();
+    }
+  });
+
+function calcularPeso(){
+   
+    const pesoObtenido = parseInt(ENTRADA.value);
+
     let volumen ;
     let mantenimiento;
     let mm;
 
-    if(isNaN(peso_obtenido)){
-         
-        
+    if((isNaN(pesoObtenido)) || pesoObtenido == 0){
          ERROR.style.display = 'block';
          MAN.innerHTML = null;
          FLU.innerHTML = null;
-    }else{
-        if( peso_obtenido <= 10){
-            volumen  = (peso_obtenido*100);
-        }else if( peso_obtenido <= 20 ){
-            volumen  = ( 10 * 100 + (peso_obtenido - 10) * 50 );
-        }else if( peso_obtenido <= 30 ){
-            volumen  = ( 10 * 100 + 10 * 50 + (peso_obtenido - 20 ) * 20 );
-        }else {
-            volumen  = ( ((peso_obtenido * 4) + 7) / (peso_obtenido + 90)*1500 );
-        }
-        mantenimiento = (volumen/24);
-        mm = (mantenimiento*1.5);
-        MAN.innerHTML = ` ${mantenimiento.toFixed(2)} cc/hr `;
-        FLU.innerHTML = `m+m/2 ${mm.toFixed(2)} cc/hr `
-        ERROR.style.display = null;
-    }   
+         return;
+        
+    }
+    if( pesoObtenido <= 10){
+        volumen  = (pesoObtenido*100);
+    }else if( pesoObtenido <= 20 ){
+        volumen  = ( 10 * 100 + (pesoObtenido - 10) * 50 );
+    }else if( pesoObtenido <= 30 ){
+        volumen  = ( 10 * 100 + 10 * 50 + (pesoObtenido - 20 ) * 20 );
+    }else {
+        volumen  = ( ((pesoObtenido * 4) + 7) / (pesoObtenido + 90)*1500 );
+    }
+    
+    mantenimiento = (volumen/24);
+    mm = (mantenimiento*1.5);
+    MAN.innerHTML = ` ${mantenimiento.toFixed(2)} cc/hr `;
+    FLU.innerHTML = `m+m/2 ${mm.toFixed(2)} cc/hr `
+    ERROR.style.display = null;
+    
 }
